9e7762e31a6df8d7524a808a67de441057fd3b94
Lesueur Benjamin <valkiriebc@hotmail.fr>
2021-01-15 18:40:47 +0100

    fixed a typo on InitPatching()

--- src/realmd/AuthSocket.cpp
+++ src/realmd/AuthSocket.cpp
@@ -187,7 +187,7 @@ std::array<uint8, 16> VersionChallenge = { { 0xBA, 0xA3, 0x1E, 0x99, 0xA0, 0x0B,
 
 /// Constructor - set the N and g values for SRP6
 AuthSocket::AuthSocket(boost::asio::io_service& service, std::function<void(Socket*)> closeHandler)
-    : Socket(service, std::move(closeHandler)), _status(STATUS_CHALLENGE), _build(0), _accountSecurityLevel(SEC_PLAYER)
+    : pPatch(NULL), _patcher(NULL), Socket(service, std::move(closeHandler)), _status(STATUS_CHALLENGE), _build(0), _accountSecurityLevel(SEC_PLAYER)
 {
     N.SetHexStr("894B645E89E1535BBDAD5B8B290650530801B18EBFBF5E8FAB3C82872A3E9BB7");
     g.SetDword(7);
@@ -1272,7 +1272,7 @@ bool Patcher::InitPatching(uint16 _build, std::string _locale, AuthSocket* _Auth
         }
         else
         {
-            path << PATCH_PATH << _build << "-" << _locale << ".mpq";
+            path << PATCH_PATH << _build << _locale << ".mpq";
         }
 
         _AuthSocket->pPatch = fopen(path.str().c_str(), "rb");
